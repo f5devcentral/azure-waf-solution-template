c237d77c542f7ad9e0bdd08f7b1abdb76406aa12
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-07-01 11:36:19 +0530

    Update deploy.py

--- Tests/deploy.py
+++ Tests/deploy.py
@@ -1,5 +1,5 @@
 import time, os, sys
-path= "/home/runner/work/azure-waf-solution-template-/azure-waf-solution-template-"
+path= "/home/runner/work/azure-waf-solution-template/azure-waf-solution-template"
 sys.path.insert(0, path)
 from Lib.utils import *
 from Lib.var import *
