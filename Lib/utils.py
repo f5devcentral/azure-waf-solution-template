fc6524fcd1f308acfe5c63e7246592f8c36d7e63
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-07-08 08:50:49 +0530

    Update utils.py

--- Lib/utils.py
+++ Lib/utils.py
@@ -34,7 +34,7 @@ def validate_user_params():
         if "ResourceNotFound" in get_vnet:
             print(azure_user_data["virnetworkId"] , "is not exists!!, Creating the same")
             create_vnet= az_get_cmd_op(create_vnet_cmd)
-        elif "Succeeded" not in get_wrkspace:
+        elif "Succeeded" not in get_vnet:
             create_vnet= az_get_cmd_op(create_vnet_cmd)
         get_vnet2= az_get_cmd_op(vnet_show)
         if "ResourceNotFound" in get_vnet2:
